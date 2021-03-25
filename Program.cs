214cf8197a90efb5a6ad43ec0143f20668cd8a0f
Kestas Jasinevicius <kestasj@comcast.net>
2021-03-25 15:06:12 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Edge.SeleniumTools;
+using OpenQA.Selenium;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SeleniumEdgeCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            var options = new EdgeOptions();
+            options.UseChromium = true;
+
+            IWebDriver edge = new EdgeDriver(
+               Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options);
+
+            edge.Manage().Window.Maximize();
+            edge.Navigate().GoToUrl("https://www.google.com/");
+            edge.Quit();
+
+        }
+    }
+}
